6c0d2c246b2f6a76d017e657aeeace7db75db30e
marwa-hamza <hamzamarwa54@gmail.com>
2022-01-03 00:36:08 +0100

    Crud final modification

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'delayed_animation.dart';
+import 'welcome_page.dart';
+const d_red = const Color(0xFFE9717D);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily App',
+      debugShowCheckedModeBanner: false,
+      home:WelcomePage()
+    );
+  }
+}
+
